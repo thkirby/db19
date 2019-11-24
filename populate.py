66ba66c489bee48cc2cdb6b414e4880ccf763268
Declan Walsh <declanpwalsh1@gmail.com>
2019-11-24 15:05:01 -0600

    Declan's commits

--- populate.py
+++ populate.py
@@ -4,9 +4,11 @@ conn = psycopg2.connect("dbname=edison user=postgres")
 cur = conn.cursor()
 
 submition_array = ["review", "complaint"]
-for i in range(1,100):
+for i in range(1, 100):
     subType = submition_array[random.randint(0, 1)]
     cur.execute("INSERT INTO Submission (subID, custID, typeofSub) VALUES (%s, %s, %s)",
     (i, random.randint(0, 999), subType))
+
+
 
 
