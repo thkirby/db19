e8561a01d5de4dce4e8cd143384df85374a8ec68
Declan Walsh <declanpwalsh1@gmail.com>
2019-11-24 17:58:01 -0600

    another commit... bugs in forms

--- mainApp.py
+++ mainApp.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+
+    return render_template("index.html")
+
+
+@app.route("/user", methods=['get', 'post'])
+def user_profile():
+
+    if "creditCard" not in request.form:
+        return render_template("user_profile.html")
+
+    else:
+        creditCard = int(request.form['creditCard'])
+        phoneNumber = int(request.form['phoneNumber'])
+        email = request.form(['email'])
+        rideDrive = request.form(['rideDrive'])
+        u = email+rideDrive
+        return render_template("user_profile.html", userName=u,creditCard=creditCard, phoneNumber=phoneNumber, email=email, rideDrive=rideDrive)
