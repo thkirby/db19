3e9b1c65a80f05a29b06cc77c912c5866527bd1e
Declan Walsh <declanpwalsh1@gmail.com>
2019-11-26 09:47:27 -0600

    updates to user_profile/mainApp.py

--- mainApp.py
+++ mainApp.py
@@ -1,23 +1,86 @@
-from flask import Flask, render_template, request
+import os
+import psycopg2
+import psycopg2.extras
+from flask import Flask, request, render_template, g
+
+# PostgreSQL IP address
+IP_ADDR = "104.197.159"
+# Create the application
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-
     return render_template("index.html")
 
 
 @app.route("/user", methods=['get', 'post'])
 def user_profile():
 
-    if "creditCard" not in request.form:
-        return render_template("user_profile.html")
+    if "step" not in request.form:
+        return render_template("user_profile.html", step="compose_entry")
 
-    else:
-        creditCard = int(request.form['creditCard'])
-        phoneNumber = int(request.form['phoneNumber'])
-        email = request.form(['email'])
-        rideDrive = request.form(['rideDrive'])
-        u = email+rideDrive
-        return render_template("user_profile.html", userName=u,creditCard=creditCard, phoneNumber=phoneNumber, email=email, rideDrive=rideDrive)
+    elif request.form["step"] == "create_user":
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("insert into customer (custid, firstname, lastname, accountinfo, phonenumber, email) values (%d, %s, %s, %s, %s, %s)", [request.form['firstName'], request.form['lastName'],request.form['accountinfo'],request.form['phonenumber'], request.form['email']])
+
+        db.commit()
+        return render_template("user_profile.html", step="create_user")
+
+
+#####################################################
+# Database handling
+
+
+def connect_db():
+    """Connects to the database."""
+    debug("Connecting to DB.")
+    conn = psycopg2.connect(host=IP_ADDR, user="postgres", password="rhodes", dbname="edisondb", 
+        cursor_factory=psycopg2.extras.DictCursor)
+    return conn
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'pg_db'):
+        g.pg_db = connect_db()
+    return g.pg_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database automatically when the application
+    context ends."""
+    debug("Disconnecting from DB.")
+    if hasattr(g, 'pg_db'):
+        g.pg_db.close()
+
+######################################################
+# Command line utilities 
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().execute(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def init_db_command():
+    """Initializes the database."""
+    print("Initializing DB.")
+    init_db()
+
+#####################################################
+# Debugging
+
+
+def debug(s):
+    """Prints a message to the screen (not web browser) 
+    if FLASK_DEBUG is set."""
+    if app.config['DEBUG']:
+        print(s)
